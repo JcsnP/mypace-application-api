9c2b94f97d9c9fcaa4b3bbda40689a8da9c05625
Chitsanupong Paenyoi <37466531+JcsnP@users.noreply.github.com>
2022-11-12 13:40:29 +0700

    FIX: push wrong respository

--- server.js
+++ server.js
@@ -20,6 +20,13 @@ mongoose.connection.on('error', (err) => {
 app.use(express.json());
 app.use(cors());
 
+ app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+  res.header("Access-Control-Allow-Headers", "x-access-token, Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
+
 // =============== Users ===============
 // create user
 app.post('/users', async (req, res) => {
