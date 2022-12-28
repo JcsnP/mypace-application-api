b9b2e08530a3e574ae8ee1ff41dd2fdb799383d8
Chitsanupong Paenyoi <37466531+JcsnP@users.noreply.github.com>
2022-12-28 15:31:28 +0700

    add new api for badges

--- routers/user_badges.js
+++ routers/user_badges.js
@@ -10,6 +10,7 @@ const Badges = require('../schemas/Badges');
 const UserBadges = require('../schemas/UserBadges');
 
 // ดึงข้อมูล badge ของผู้ใช้คนใดคนนึง
+/*
 router.get('/users/me/badges', async(req, res) => {
   try {
     const token = req.headers.authorization.split(' ')[1];
@@ -45,6 +46,51 @@ router.get('/users/me/badges', async(req, res) => {
     res.json({status: 404, message: 'can\'t find user\'s badges', error: error.message});
   }
 });
+*/
+
+// ดึงข้อมูล badge ของผู้ใช้คนใดคนนึง พร้อมสถานะ
+/*
+router.get('/users/me/badges', async(req, res) => {
+  try {
+    const token = req.headers.authorization.split(' ')[1];
+    var iss = jwt.verify(token, SECRET).iss;
+
+    const badge = await Badges.aggregate([
+      {
+        '$lookup': {
+          'from': UserBadges.collection.name,
+          'localField': "_id",
+          'foreignField': "badge_id",
+          'as': "badge"
+       }
+      },
+      {
+        '$unwind': {'path': '$badge', 'preserveNullAndEmptyArrays': true}
+      },
+      {
+        '$match': {
+          '$or': [
+            {'badge.user_id': mongoose.Types.ObjectId(iss)},
+            {'badge': {'$exists': false}},
+          ]
+        }
+      },
+      {
+        '$project': {
+          '_id': 0,
+          'createdAt': 0,
+          'updatedAt': 0,
+          'badge.createdAt': 0,
+          'badge.updatedAt': 0,
+        }
+      }
+    ])
+    res.json({status: 200, badges: badge});
+  } catch(error) {
+    res.json({status: 404, message: 'can\'t find user\'s badges', error: error.message});
+  }
+});
+*/
 
 // สร้าง badge ของผู้ใช้คนใดคนนึง
 router.post('/users/me/badges', async(req, res) => {
@@ -53,7 +99,7 @@ router.post('/users/me/badges', async(req, res) => {
     var iss = jwt.verify(token, SECRET).iss;
     const payload = req.body;
     // เช็คก่อนว่าปลดล็อกแล้วยัง
-    if(await UserBadges({user_id: iss, badge_id: payload.badge_id})) {
+    if(await UserBadges.findOne({user_id: iss, badge_id: payload.badge_id})) {
       return res.json({status: 406, message: 'alreay unlocked'});
     }
 
